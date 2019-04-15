22e8c18bf33efeb7f22169d62c27e1f1e33a0558
S1x6 <alltoall98@mail.ru>
2019-04-15 22:37:50 +0700

    added email

---
+++ WebTask4/Migrations/20190415153539_Email.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebTask4.Migrations
+{
+    public partial class Email : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                nullable: false,
+                defaultValue: "example@domain.com");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+        }
+    }
+}
